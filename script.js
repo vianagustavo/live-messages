8c7d24c1e1425dc495ae314f311435d593f6d05b
Gustavo <gustavo.ferreira@kinvo.com.br>
2023-07-11 18:40:48 -0300

    chore: add timer logic to script.js

--- script.js
+++ script.js
@@ -21,9 +21,24 @@ function displayMessages() {
   messageContainer.innerHTML = "";
 
   messages.forEach((message) => {
-    const messageElement = document.createElement("div");
-    messageElement.textContent = message;
+    const now = new Date();
+    const messageHours = now.getHours();
+    const messageMinutes = now.getMinutes();
 
-    messageContainer.appendChild(messageElement);
+    const timerMessage = `${messageHours}:${messageMinutes}`;
+    const timerMessageDiv = document.createElement("div");
+    timerMessageDiv.textContent = timerMessage;
+    timerMessageDiv.setAttribute("class", "timerMessage");
+
+    const elementMessageDiv = document.createElement("div");
+    elementMessageDiv.textContent = message;
+    elementMessageDiv.setAttribute("class", "elementMessage");
+
+    const messageWrapper = document.createElement("div");
+    messageWrapper.setAttribute("class", "messageWrapper");
+    messageWrapper.appendChild(elementMessageDiv);
+    messageWrapper.appendChild(timerMessageDiv);
+
+    messageContainer.appendChild(messageWrapper);
   });
 }
